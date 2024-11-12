c2d3595de01c17142fa767bcd4e6ff77fa57f048
Syco King <89406307+sycokingk@users.noreply.github.com>
2024-11-13 00:20:38 +0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const config = {
   SESSION_ID: process.env.SESSION_ID || "Ethix-MD&WvrfSqMp",
-  PREFIX: process.env.PREFIX || '.',
+  PREFIX: process.env.PREFIX || '/',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'false' : false,
   STATUS_READ_MSG: process.env.STATUS_READ_MSG || '',
